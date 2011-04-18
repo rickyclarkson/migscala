99dd33d15e0f008d7b052b7b9b3eff96c77417c4
Ricky Clarkson <richard.clarkson@globant.com>
2011-04-18 14:51:06 -0300

    Making an example that uses MigLayout with Scala Swing, but still just with Strings for constraints.

--- src/main/scala/migscala/MigScala.scala
+++ src/main/scala/migscala/MigScala.scala
@@ -1,10 +1,26 @@
 import scala.swing._
+import javax.swing.JPanel
+import net.miginfocom.swing.MigLayout
 
 object HelloWorld extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "Hello, World!"
-    contents = new Button {
-      text = "Click Me!"
+    contents = {
+      val panel = new MigPanel
+      panel.layout(new Label("Foo")) = ""
+      pack
+      panel
     }
   }
+}
+
+class MigPanel extends Panel with LayoutContainer {
+  type Constraints = String
+  override lazy val peer = new JPanel(new MigLayout)
+  private def layoutManager = peer.getLayout.asInstanceOf[MigLayout]
+
+  protected def constraintsFor(comp: Component) = layoutManager.getComponentConstraints(comp.peer).asInstanceOf[String]
+  
+  protected def areValid(c: Constraints): (Boolean, String) = (true, "")
+  def add(c: Component, const: String) { peer.add(c.peer, const) }
 }
